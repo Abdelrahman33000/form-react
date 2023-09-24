b1d0580aa22407c6c8c4164fc0aafafac63dcadd
abdo <aboabdo8600@hmail.com>
2023-09-24 13:55:27 +0300

    react-form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import FileBase from 'react-file-base64';
+import React, { useState } from 'react';
 
-function App() {
+function Form() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [file, setFile] = useState(null);
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    setUsername('');
+    setPassword('');
+    setFile(null);
+    setConfirmPassword('');
+    setRememberMe(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <br />
+<label>
+  Upload File:
+  <FileBase
+    type="file"
+    multiple={false}
+    onDone={({ base64 }) => setFile(base64)}
+  />
+</label>
+      <br />
+      <label>
+        Confirm Password:
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        <input
+          type="checkbox"
+          checked={rememberMe}
+          onChange={(e) => setRememberMe(e.target.checked)}
+        />
+        Remember me
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
-export default App;
+export default Form;
+
+
+
+
+
+
+
